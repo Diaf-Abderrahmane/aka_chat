ac6c18d69d2344358c91fef73c7f3f63ca9d29bb
Shoyoo01 <ja_diaf@esi.dz>
2024-02-10 21:29:27 +0100

    login page

--- front/lib/pages/login.dart
+++ front/lib/pages/login.dart
@@ -1,12 +1,93 @@
+import 'package:aka_chat/components/my_button.dart';
+import 'package:aka_chat/components/my_textfield.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _pwlController = TextEditingController();
+
+  LoginPage({super.key});
+
+  //login method
+  void login() {
+    //authentication
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
+      body: Column(
+        children: [
+          //logo
+          Icon(
+            Icons.message,
+            size: 60,
+          ),
+
+          const SizedBox(
+            height: 50,
+          ),
+
+          //Welcome back message
+          Text(
+            "Welcome Back Man !",
+            style: TextStyle(
+                color: Theme.of(context).colorScheme.primary, fontSize: 16),
+          ),
+
+          const SizedBox(
+            height: 25,
+          ),
+
+          //email textfield
+          MyTextField(
+            hintText: "Email",
+            obscureText: false,
+            controller: _emailController,
+          ),
+
+          SizedBox(
+            height: 10,
+          ),
+
+          //password textfield
+          MyTextField(
+            hintText: "Password",
+            obscureText: true,
+            controller: _pwlController,
+          ),
+
+          SizedBox(
+            height: 25,
+          ),
+
+          //login button
+          MyButton(
+            text: "Login",
+            onTap: login,
+          ),
+
+          SizedBox(
+            height: 25,
+          ),
+
+          //register now text
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Not a member?",
+                style: TextStyle(color: Theme.of(context).colorScheme.primary),
+              ),
+              Text(
+                "Register now",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
